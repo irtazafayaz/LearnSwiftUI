e2cd608db29c13feecc92222338c324dff0ac80c
irtazafayaz <irtazab135@gmail.com>
2024-09-30 00:12:36 +0500

    Backend part of CoreML bettersleep

--- LearnSwiftUI/BetterRest/BetterRest.swift
+++ LearnSwiftUI/BetterRest/BetterRest.swift
@@ -13,6 +13,10 @@ struct BetterRest: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
+    @State private var alertTitle: String = ""
+    @State private var alertMessage: String = ""
+    @State private var showAlert: Bool = false
+
     var body: some View {
         NavigationStack {
             VStack {
@@ -32,15 +36,38 @@ struct BetterRest: View {
                 
                 Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
             }
+            .padding()
             .navigationTitle("BetterRest")
             .toolbar {
                 Button("Calculate", action: calculateBedTime)
+            }
+            .alert(alertTitle, isPresented: $showAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
             }
         }
     }
     
     private func calculateBedTime() {
-        
+        do {
+            let configuration = MLModelConfiguration()
+            let sleepCalculator = try SleepCalculator(configuration: configuration)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hourInSeconds = (components.hour ?? 0) * 60 * 60
+            let minutesInSeconds = (components.minute ?? 0) * 60
+            
+            let prediction = try sleepCalculator.prediction(wake: Double(hourInSeconds + minutesInSeconds), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepRequired = wakeUp - prediction.actualSleep
+            alertTitle = "You should go to bed at..."
+            alertMessage = sleepRequired.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Oops, couldn't find the sleep time required."
+        }
+        showAlert.toggle()
     }
 }
 
